8734347ed7e4ba5ef2169e44106bd56200ad72c4
Dirk Eddelbuettel <edd@debian.org>
2015-06-22 07:29:57 -0400

    examples for U Zuerich course

---
+++ uzuerich-2015-06/numVecEx1.cpp
@@ -0,0 +1,13 @@
+
+#include <Rcpp.h>
+
+library(inline)
+
+// [[Rcpp::export]]
+double numVecEx1(Rcpp::NumericVector vec, double p) {
+    double sum = 0.0;
+    for (int i=0; i<vec.size(); i++) {
+        sum += pow(vec[i], p);
+    }
+    return sum;
+}
---
+++ uzuerich-2015-06/numVecEx2.cpp
@@ -0,0 +1,13 @@
+
+#include <Rcpp.h>
+
+// [[Rcpp::export]]
+Rcpp::DataFrame numVecEx2(Rcpp::NumericVector xs) {
+    Rcpp::NumericVector x1(xs);
+    Rcpp::NumericVector x2(Rcpp::clone(xs));
+    x1[0] = 22;
+    x2[1] = 44;
+    return(Rcpp::DataFrame::create(Named("orig", xs),
+                                   Named("x1", x1),
+                                   Named("x2", x2)));
+}
